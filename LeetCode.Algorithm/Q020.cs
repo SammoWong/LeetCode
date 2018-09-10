eb6299efcbea4ec476862ba413252bbb7880458d
Sammo <sammo.wong@foxmail.com>
2018-09-10 16:11:54 +0800

    add Q020

---
+++ LeetCode.Algorithm/Q020.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 有效的括号
+    /// </summary>
+    public class Q020
+    {
+        public bool IsValid(string s)
+        {
+            Stack stack = new Stack();
+            var array = s.ToCharArray();
+            foreach (var item in array)
+            {
+                if (item == '(')
+                    stack.Push(')');
+
+                else if (item == '[')
+                    stack.Push(']');
+
+                else if (item == '{')
+                    stack.Push('}');
+
+                else if (item == '<')
+                    stack.Push('>');
+
+                else if (stack.Count == 0 || stack.Pop().ToString() != item.ToString())
+                    return false;
+            }
+            return stack.Count == 0;
+        }
+    }
+}
