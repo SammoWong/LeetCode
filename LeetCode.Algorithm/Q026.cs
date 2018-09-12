2d29c955d57096f3cb45fd0a37edb3f38d20d09d
Sammo <sammo.wong@foxmail.com>
2018-09-12 17:59:36 +0800

    Add Q026

---
+++ LeetCode.Algorithm/Q026.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 删除排序数组中的重复项
+    /// </summary>
+    public class Q026
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
