5140c504c164f5df0fb358039199bd51751b7642
Sammo <sammo.wong@foxmail.com>
2018-09-13 14:45:13 +0800

    Add Q027

---
+++ LeetCode.Algorithm/Q027.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 移除元素
+    /// </summary>
+    public class Q027
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+
+        }
+    }
+}
