b2d1d707f145ac4f8748b372ef3e31238ac973be
Sammo <sammo.wong@foxmail.com>
2018-09-13 14:46:25 +0800

    Add Q027

--- LeetCode.Algorithm/Q027.cs
+++ LeetCode.Algorithm/Q027.cs
@@ -14,7 +14,16 @@ namespace LeetCode.Algorithm
             if (nums.Length == 0)
                 return 0;
 
-
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if(nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
         }
     }
 }
