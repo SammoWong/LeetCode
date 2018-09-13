efe656f7e41a59e1e9b2f886a3396128cd48344f
Sammo <sammo.wong@foxmail.com>
2018-09-13 17:23:32 +0800

    Add Q028

---
+++ LeetCode.Algorithm/Q028.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 实现strStr()
+    /// </summary>
+    public class Q028
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle))
+                return 0;
+
+            for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] == needle[j])
+                    {
+                        if (j == needle.Length - 1)
+                            return i;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
