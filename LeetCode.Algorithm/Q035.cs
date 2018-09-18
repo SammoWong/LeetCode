8cc5d14371890aad5a8441b501dea235e8365b41
Sammo <sammo.wong@foxmail.com>
2018-09-18 16:51:18 +0800

    Add Q035

---
+++ LeetCode.Algorithm/Q035.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 搜索插入位置
+    /// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+    /// </summary>
+    public class Q035
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (target <= nums[i])
+                {
+                    return i;
+                }
+            }
+            return nums.Length;
+        }
+    }
+}
