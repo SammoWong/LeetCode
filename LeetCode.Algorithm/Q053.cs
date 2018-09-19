43af7f94fd46829e92544c8d29803e4c03c05b1e
Sammo <sammo.wong@foxmail.com>
2018-09-19 15:29:35 +0800

    Add Q053

---
+++ LeetCode.Algorithm/Q053.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 最大子序和
+    /// </summary>
+    public class Q053
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            var max = nums.Max();
+            int result = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                result = result + nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    result += nums[j];
+                    if (result > max)
+                        max = result;
+                }
+                result = 0;
+            }
+            return max;
+        }
+    }
+}
