84fcfdd68da700be83d89e5618aea40036d0cb60
Sammo <sammo.wong@foxmail.com>
2019-04-30 16:32:38 +0800

    Add Q101

---
+++ LeetCode.Algorithm/Q101.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 对称二叉树
+    /// 给定一个二叉树，检查它是否是镜像对称的
+    /// </summary>
+    public class Q101
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return Judge(root, root);
+        }
+
+        public bool Judge(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+            if (left == null || right == null)
+                return false;
+            if (left.val != right.val)
+                return false;
+            return Judge(left.left, right.right) && Judge(left.right, right.left);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
