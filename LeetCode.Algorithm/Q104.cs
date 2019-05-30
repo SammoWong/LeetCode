9c840f37101b301487a28689393b029260386791
Sammo <sammo.wong@foxmail.com>
2019-05-30 14:57:24 +0800

    Add Q104

---
+++ LeetCode.Algorithm/Q104.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    public class Q104
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            var left = MaxDepth(root.left);
+            var right = MaxDepth(root.right);
+            return left > right ? left + 1 : right + 1;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
