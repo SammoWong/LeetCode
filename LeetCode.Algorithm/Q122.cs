cb21e3af23dee398891ec7a565056dbe862679ae
Sammo <sammo.wong@outlook.com>
2020-03-06 17:23:47 +0800

    Add Q122

---
+++ LeetCode.Algorithm/Q122.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 买卖股票的最佳时机 II
+    /// </summary>
+    class Q122
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+            }
+
+            return maxProfit;
+        }
+    }
+}
