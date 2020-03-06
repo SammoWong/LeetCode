8154d945c4e7003383b4a6dc412a0813fed4c54e
Sammo <sammo.wong@outlook.com>
2020-03-06 16:27:29 +0800

    Add Q121

---
+++ LeetCode.Algorithm/Q121.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    /// <summary>
+    /// 买卖股票的最佳时机
+    /// </summary>
+    public class Q121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    var profit = prices[j] - prices[i];
+                    if (profit > maxProfit)
+                        maxProfit = profit;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
